073809dd08b52bd6f50dc20d964c53c57e1bff8b
Sulla <sulla2357@email.com>
2019-07-31 01:34:46 +0900

    Restart frontend

---
+++ src/Components/Router.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "../Routes/Login";
+import Feed from "../Routes/Feed";
+
+const LoggedInRoutes = () => (
+  <>
+    <Route exact path="/" component={Feed} />
+  </>
+);
+
+const LoggedOutRoutes = () => (
+  <>
+    <Route exact path="/" component={Login} />
+  </>
+);
+
+const AppRouter = ({ isLoggedIn }) => (
+  <Router>
+    <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
+  </Router>
+);
+
+AppRouter.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+export default AppRouter;
